b159d4730b5f7dad89145b0dbb2eb955d05cf00a
Javier Corona <57466810+JavierCoronaA01023063@users.noreply.github.com>
2022-04-04 19:52:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="A01023063_App">
-      <header className="App-header">
+      <header className="A01023063_App">
         <img src={https://imgs.search.brave.com/bhyN62lm8kx7xhKOui7Yavvi93vV4tpwpFxAKOl8lY8/rs:fit:1200:1200:1/g:ce/aHR0cDovL2kuaW1n/dXIuY29tL1NZM2F2/ZjgucG5n} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
