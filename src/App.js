cf09e7d1c28a22bd6587ecb45b8fb67dc0a92665
Javier Corona <57466810+JavierCoronaA01023063@users.noreply.github.com>
2022-04-04 19:54:20 -0500

    Update App.js
    Added dodgecoin img

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="A01023063_App">
       <header className="A01023063_App">
-        <img src={imgs.search.brave.com/bhyN62lm8kx7xhKOui7Yavvi93vV4tpwpFxAKOl8lY8/rs:fit:1200:1200:1/g:ce/aHR0cDovL2kuaW1n/dXIuY29tL1NZM2F2/ZjgucG5n} className="App-logo" alt="logo" />
+        <img src="https://imgs.search.brave.com/8Mf_LiJz8-v_3UMcZ8LoRrXejDW9L-0SmvDLtC8er6k/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9leHRl/cm5hbC1wcmV2aWV3/LnJlZGQuaXQvSjVK/ZGRmTlFocGtOeFYt/WGE5Y1MwcURORTZK/RU13ZG92Znd6U1FC/VXVpcy5wbmc_YXV0/bz13ZWJwJnM9YTZk/MjhhZTc2YmM1Mzhk/NzUxYjc5NzZjNmU5/ZDAwOTVjMzcxYzA3/MA" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
